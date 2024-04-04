bc8d3ff9a4e0e968f6bab5b3b3cad0680e959fd0
chagchagchag <chagchagchag.dev@gmail.com>
2024-04-04 15:51:52 +0900

    first status

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Docs Spring Reactive Test</span>,
+  project: {
+    link: 'https://github.com/chagchagchag/docs-spring-reactive-test',
+  },
+  docsRepositoryBase: 'https://github.com/chagchagchag/docs-spring-reactive-test',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
